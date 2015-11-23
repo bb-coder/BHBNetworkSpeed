8849bda9474f0e3b353a4a3576b561bfbdcc6dbc
bihongbo <bihongbo@jiyoutang.com>
2015-11-23 22:52:47 +0800

    version1.0

---
+++ BHBNetworkSpeedDemo/BHBNetworkSpeedDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BHBNetworkSpeedDemo
+//
+//  Created by bihongbo on 15/11/19.
+//  Copyright © 2015年 bihongbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BHBNetworkSpeedDemo/BHBNetworkSpeedDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BHBNetworkSpeedDemo
+//
+//  Created by bihongbo on 15/11/19.
+//  Copyright © 2015年 bihongbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
